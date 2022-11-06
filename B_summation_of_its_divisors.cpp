e0a7cfee71ec1010fb59c053683fe41277bdf45d
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2022-11-06 20:28:32 +0200

    solution of the problem B

---
+++ B_summation_of_its_divisors.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define TC  int t; cin >> t; while (t--) 
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define cout(v) for (auto &x : v) cout << x << ' ';
+#define cin(v)  for (auto &x : v) cin >> x;
+#define prefix(v, n) for (int i = 1; i < n; i++) v[i] += v[i - 1];
+#define eps 1e-7
+using namespace std;
+                
+                /******functions Definition******/
+inline void in_out();
+inline void solve();
+ll ans(ll n){
+    ll sum=0;
+    for(ll i=1;i*i<=n;i++){
+        if(n%i==0){
+            if(n/i==i)
+                sum+=i;
+            else{
+                sum+=i;
+                sum+=(n/i);
+            }
+        }
+    }
+    return sum;
+}
+  
+int main(){
+   in_out();  
+    //TC                                        
+    solve(); 
+   return 0;
+}
+                                    /******functions Declaration******/               
+inline void in_out(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie();    
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt","r",stdin);   
+        freopen("out.txt","w",stdout);
+    #endif  
+}   
+inline  void solve(){
+ ll n;  cin>>n;
+ cout<<ans(n);
+
+}
